f1f2cb5bf14d98ec3719088f3cd58d49bed4caee
omcewan <orlandomcewan231@gmail.com>
2021-12-12 09:47:33 -0500

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,33 +3,28 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-console.log(playerName, playerHealth, playerAttack);
-
-var enemyName = "Roborto";
+var enemyNames = ["Roberto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-console.log(enemyName, enemyAttack, enemyHealth);
-
-var fight = function(){
+var fight = function(enemyName){
     window.alert("Welcome to Robot Gladiators");
 
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
-// if players chooses fight then fight
+    // if players chooses fight then fight
     if(promptFight === "FIGHT" || promptFight === "fight"){
-
         //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
         enemyHealth = enemyHealth - playerAttack;
-    
         // Log a resulting message to the console so we know that it worked.
         console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
-
+        
         if(enemyHealth <= 0){
             window.alert(enemyName + " has died!");
         }else{
             window.alert(enemyName + " still has " + enemyHealth + " health left.");
         }
+
         // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
         playerHealth = playerHealth - enemyAttack;
     
@@ -49,7 +44,6 @@ var fight = function(){
 
         if(confirmSkip){
             window.alert(playerName + " has decided to skip this fight. Goodbye");
-
             // subtract money from playerMoney for skipping the round.
             playerMoney = playerMoney - 2;
             console.log(playerMoney);
@@ -59,5 +53,15 @@ var fight = function(){
     }else{
         window.alert("You need to choose a valid option. Try Again!");
     }
+}
 
-fight();
+for(var i = 0; i < enemyNames.length; i++){
+    fight(enemyNames[i]);
+}
+// fight();
+
+// Game States
+// "WIN" - Player robot has defeated all enemy-robots
+//    * Fight all enemy-robots
+//    * Defeat each enemy-robot
+// "LOSE" - Player robot's health is zero or less
